c08329669e2a331b5b11a9c4fdfe712d78fb126d
KIRENGA-Remy <gitoliremy@gmail.com>
2026-04-21 19:02:00 +0200

    Laravel models

---
+++ database/migrations/2026_04_21_143758_create_subjects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject_code', 10);
+            $table->string('subject_name', 50);
+            $table->string('category', 10);
+            $table->foreignId('teacher_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
